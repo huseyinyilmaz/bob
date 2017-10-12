5b4030ec2373f04475e8428d345af72d24fbaae4
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2017-10-12 11:38:59 -0700

    Remove unused imports

--- library/Command.hs
+++ library/Command.hs
@@ -1,10 +1,5 @@
 module Command (main) where
-
-import Turtle
 import Env
 
-import Control.Monad.Reader(ReaderT)
-import Control.Monad.Reader(runReaderT)
-import Data.Maybe(fromMaybe)
-
+main :: IO ()
 main = readData
